b4ca4a5c882128ddaadef97c93ef32bd8554d520
rightvalue <bluebird6922@gamil.com>
2021-02-23 18:22:31 +0900

    initial

---
+++ consts/env.go
@@ -0,0 +1,11 @@
+package consts
+
+const (
+	MYSQL_USER     = "root"
+	MYSQL_PASSWORD = ""
+	MYSQL_HOST     = "localhost"
+	EMAIL_FROM     = "EMAIL_FROM"
+	EMAIL_HOST     = "EMAIL_HOST"
+	EMAIL_PASSWORD = "EMAIL_PASSWORD"
+	JWT_SECRET     = "JWT_SECRET"
+)
